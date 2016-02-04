89aad6363b58949198b31440edddc9becc54c4ae
JosephLandy <joseph.landy.email@gmail.com>
2016-02-03 21:06:33 -0500

    important update to .gitignore
    Set git ignore so that standard assets folder and editor folder will not
    be tracked.

---
+++ holepuncher/Assets/testScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //meaningless test edit.
+	
+	}
+}
