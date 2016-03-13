da2009eb758a0a9c236e0d7603650160f2ffbd9e
JosephLandy <joseph.landy.email@gmail.com>
2016-03-13 18:03:22 -0400

    attempted to improve things

---
+++ holepuncher/Assets/scripts/NonStickCoating.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonStickCoating : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D col) {
+        Rigidbody2D incphys = col.gameObject.GetComponent<Rigidbody2D>();
+        incphys.velocity = new Vector2(0f, incphys.velocity.y);
+    }
+
+}
