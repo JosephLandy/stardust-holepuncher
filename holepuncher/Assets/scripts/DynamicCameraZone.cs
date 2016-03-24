04337f64247bf0dbc20e9882e074180dd0f35034
Alex <mersereau.alex@gmail.com>
2016-03-24 01:08:32 -0400

    Dynamic Camera is now more dynamic
    Though it might break some scenes. For the main scene, the important
    values are x = 32.5, y = -5.2, and scale = 6.5

---
+++ holepuncher/Assets/scripts/DynamicCameraZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicCameraZone : MonoBehaviour {
+	private fakeDynamicCamera cameraScript;
+
+	public Vector2 centerPosition;		//use this to determine where the camera will go when the player enters this area
+	public float scaleFactor;			//use this to determine how far the camera zooms out
+
+	void Start (){
+		cameraScript = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<fakeDynamicCamera>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			cameraScript.SetMode(1, centerPosition, scaleFactor);
+        }
+    }
+
+	void OnTriggerExit2D(Collider2D col) { 
+		if (col.gameObject.tag == "Player")
+			cameraScript.SetMode(2, centerPosition, scaleFactor);
+	}
+}
