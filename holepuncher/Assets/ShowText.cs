62280a24f20b938813565d39f9757bf8e4fb8890
JosephLandy <joseph.landy.email@gmail.com>
2016-03-26 16:07:30 -0400

    add restart prompt on death and success along with fadeouts

---
+++ holepuncher/Assets/ShowText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour {
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+	
+	}
+
+    //activate the text component to show the text.
+    public void showMeDeath() {
+        if (gameObject.name == "gameOver_text") {
+            text.enabled = true;
+        }
+    }
+
+    public void showMeWin() {
+        if (gameObject.name == "gameWon_text") {
+            text.enabled = true;
+        }
+    }
+}
---
+++ holepuncher/Assets/WaitForRestart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WaitForRestart : MonoBehaviour {
+
+    // wait for the user to press any key to restart the level. 
+	void Update() {
+        if (Input.anyKeyDown) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
