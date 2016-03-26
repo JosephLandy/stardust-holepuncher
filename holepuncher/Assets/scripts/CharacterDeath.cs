dbbb52509a756a3258707d52c823bc913dd35fb3
Alex <mersereau.alex@gmail.com>
2016-03-26 18:14:15 -0400

    trigger warning
    made character death script work with triggers so that the kill floor
    allows objects to pass through it (but, of course, not the player).

--- holepuncher/Assets/scripts/CharacterDeath.cs
+++ holepuncher/Assets/scripts/CharacterDeath.cs
@@ -28,8 +28,10 @@ public class CharacterDeath : MonoBehaviour {
 
     }
     private void OnCollisionEnter2D(Collision2D col) {
-        if (col.collider.CompareTag("Fatal")) {
-            //player has been killed.
+		if (col.collider.CompareTag("Fatal")) {
+			killme ();
+            /*
+			//player has been killed.
             // We want to keep the player graphically in the same position, but cease all movement and physics.
             // we can do this by disabling the control and movement scripts and removing the rigidbody. 
             RifterControl rc = gameObject.GetComponent<RifterControl>();
@@ -40,9 +42,14 @@ public class CharacterDeath : MonoBehaviour {
             Destroy(gameObject.GetComponent<Rigidbody2D>()); //remove the rigid body component from the game object. 
             //contact the game manager to do all the other death stuff. 
             gm.SendMessage("gameOver");
-
+			*/
 
         }
     }
+	private void OnTriggerEnter2D(Collider2D col) {		//added by Alex to make the script compatible with triggers
+		if (col.CompareTag ("Fatal")) {
+			killme ();
+		}
+	}
 
 }
