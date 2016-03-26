e9ffda4596dcb9527696bf3e192ab33f2fab1c5d
JosephLandy <joseph.landy.email@gmail.com>
2016-03-26 17:39:39 -0400

    Made lasers fatal again. Included game manager in final scene

--- holepuncher/Assets/scripts/CharacterDeath.cs
+++ holepuncher/Assets/scripts/CharacterDeath.cs
@@ -12,6 +12,21 @@ public class CharacterDeath : MonoBehaviour {
 
     }
 
+    public void killme() {
+        //player has been killed.
+        // We want to keep the player graphically in the same position, but cease all movement and physics.
+        // we can do this by disabling the control and movement scripts and removing the rigidbody. 
+        RifterControl rc = gameObject.GetComponent<RifterControl>();
+        rc.enabled = false;
+        RifterControlInterface rci = gameObject.GetComponent<RifterControlInterface>();
+        rci.enabled = false;
+
+        Destroy(gameObject.GetComponent<Rigidbody2D>()); //remove the rigid body component from the game object. 
+                                                         //contact the game manager to do all the other death stuff. 
+        gm.SendMessage("gameOver");
+
+
+    }
     private void OnCollisionEnter2D(Collision2D col) {
         if (col.collider.CompareTag("Fatal")) {
             //player has been killed.
