bbb1560be4b96b147e6e56a3fb469769efb8997c
Alex <mersereau.alex@gmail.com>
2016-03-30 01:36:27 -0400

    Super satisfying solution
    I dunno if the level is completely "done" but it's in a playable state
    and I like the way it ends.

--- holepuncher/Assets/SpontaneousWhiteHoleScript.cs
+++ holepuncher/Assets/SpontaneousWhiteHoleScript.cs
@@ -3,20 +3,15 @@ using System.Collections;
 
 public class SpontaneousWhiteHoleScript : MonoBehaviour {
 	private Vector3 spawnPoint;
-	private WhiteHoleMaker whiteHoleScript;
-	private GameObject whiteHole;
+	public GameObject holePrefab;
 
 	public void Start (){
-		whiteHoleScript = GameObject.Find ("whiteHole_parent").GetComponent<WhiteHoleMaker>();
 	}
 
 	public void OnTriggerEnter2D(Collider2D obj) {
 		if (obj.gameObject.tag == "Player") {
 			spawnPoint = obj.GetComponent<Transform> ().position;
-			whiteHoleScript.makeWhiteHole (spawnPoint);
-			whiteHole = GameObject.Find ("Whitehole(Clone)");
-			whiteHole.GetComponent<PointEffector2D> ().forceMagnitude = 1000;
-			Object.Destroy(whiteHole.GetComponentInChildren<SpriteRenderer>());
+			GameObject hole = (GameObject)Instantiate(holePrefab, spawnPoint, Quaternion.identity);
 		}
 	}
 }
